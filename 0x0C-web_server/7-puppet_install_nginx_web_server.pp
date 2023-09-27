4a8d0a552b4b919b09b58e30a6aee9f70254a56a
sakachris <sakachris90@gmail.com>
2023-09-27 18:44:39 +0300

    puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,6 @@
+# Installing and configuring nginx server
+
+exec { 'install config nginx':
+  provider => shell,
+  command  => 'apt-get -y update && apt-get -y install nginx && ufw allow "Nginx HTTP" && echo "Hello World!" > /var/www/html/index.html && service nginx start && sed -i "25i location /redirect_me {\n return 301 https://www.youtube.com/watch?v=v5nfmtFzvvk;\n}\n" /etc/nginx/sites-available/default && service nginx restart',
+}
