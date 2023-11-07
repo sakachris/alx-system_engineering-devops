def717b222709398c157f279633c102bdb17e3cd
sakachris <sakachris90@gmail.com>
2023-11-07 21:06:15 +0300

    top ten none

--- 0x16-api_advanced/top_ten.py
+++ 0x16-api_advanced/top_ten.py
@@ -12,10 +12,10 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'chris'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code >= 300:
-        print('None')
-    else:
+    if response.status_code == 200:
         data = response.json()
         posts = data['data']['children']
         for post in posts:
             print(post['data']['title'])
+    else:
+        return None
