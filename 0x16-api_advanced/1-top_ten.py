dd6470c93d45252e888d0d4a47bda3e73806f29b
sakachris <sakachris90@gmail.com>
2023-11-07 14:22:34 +0300

    top 10fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,7 +11,7 @@ def top_ten(subreddit):
     """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'chris'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         posts = data['data']['children']
