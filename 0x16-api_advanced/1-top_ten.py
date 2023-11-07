9b4cbc3186992fea77ec054acb6ba99eec016f08
sakachris <sakachris90@gmail.com>
2023-11-07 13:14:25 +0300

    top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ queries the Reddit API and returns the number of subscribers
 import requests
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """
     prints the titles of the first 10 hot posts listed for a given subreddit
     """
