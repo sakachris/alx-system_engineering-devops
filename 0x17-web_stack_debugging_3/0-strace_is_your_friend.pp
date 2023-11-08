21bbc77cd7b1530901ac3189ecba35b42b8ad107
sakachris <sakachris90@gmail.com>
2023-11-09 02:16:52 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,6 @@
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  content => file('/var/www/html/wp-settings.php').content.gsub('".phpp"', '".php"'),
-}
+# find out why Apache is returning a 500 error using strace
 
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/var/www/html/wp-settings.php'],
+file { '/var/www/html/wp-includes/class-wp-locale.phpp':
+  ensure => 'file',
+  source => '/var/www/html/wp-includes/class-wp-locale.php'
 }
